56d3d519cd92e853e7a034afa81baf60db19cfaf
Toufik khan <toufik9909@gmail.com>
2023-02-10 15:21:09 +0600

    last change

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('my name is toufik khan');
+
+let name = "Arham Mahmus Ayan "
+let name2 = "Abu Talha "
+let name3 = "Mahima Rahman"
+
+let era3vaibon = name+name2+name3;
+
+console.log(era3vaibon);
